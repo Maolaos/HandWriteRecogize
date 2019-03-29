6db0a142b0fbd8d4ee670d32150d65494523ad86
jiaoyanan <jiao_ya_nan@163.com>
2019-03-29 11:36:19 +0800

    Initial commit

---
+++ app/src/main/java/com/jyn/handwriterecogizetest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jyn.handwriterecogizetest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jyn.handwriterecogize.InputViewActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent =new Intent(this,InputViewActivity.class);
+        startActivity(intent);
+    }
+}
